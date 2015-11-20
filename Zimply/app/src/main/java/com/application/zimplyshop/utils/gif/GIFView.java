7dd8b9069ab887a8d1fb00464e6b5677a876ac06
Apoorv Arora <apoorv@zimply.in>
2015-11-20 17:41:33 +0530

    Gif decoder

---
+++ Zimply/app/src/main/java/com/application/zimplyshop/utils/gif/GIFView.java
@@ -0,0 +1,38 @@
+package com.application.zimplyshop.utils.gif;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Movie;
+import android.os.SystemClock;
+import android.view.View;
+
+import java.io.InputStream;
+
+public class GIFView extends View {
+	private Movie mMovie;
+
+	private long mMoviestart;
+
+	public GIFView(Context context, InputStream stream) {
+		super(context);
+		mMovie = Movie.decodeStream(stream);
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawColor(Color.TRANSPARENT);
+		super.onDraw(canvas);
+		final long now = SystemClock.uptimeMillis();
+
+		if (mMoviestart == 0) {
+			mMoviestart = now;
+		}
+
+		final int relTime = (int) ((now - mMoviestart) % mMovie.duration());
+		mMovie.setTime(relTime);
+		mMovie.draw(canvas, 10, 10);
+		this.invalidate();
+	}
+
+}
