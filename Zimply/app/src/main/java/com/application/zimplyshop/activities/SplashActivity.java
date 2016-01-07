7ded86a615ef524c4a99028c74fb4a3f79ef3a31
Saurabh <svvashishtha@gmail.com>
2016-01-07 18:55:24 +0530

    pta ni kya kia

--- Zimply/app/src/main/java/com/application/zimplyshop/activities/SplashActivity.java
+++ Zimply/app/src/main/java/com/application/zimplyshop/activities/SplashActivity.java
@@ -18,7 +18,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -192,14 +191,14 @@ public class SplashActivity extends BaseActivity implements RequestTags, GetRequ
         //
         appConfig();
 
-        try {
+        /*try {
            // imageView.setImageResource(R.drawable.ic_splash);
             imageView.setImageBitmap(CommonLib.getBitmap(this, R.drawable.ic_splash, width, height));
         } catch (OutOfMemoryError e) {
             imageView.setImageBitmap(CommonLib.getBitmap(this, R.drawable.ic_splash, width, height));
         }
-
-        imageView.setVisibility(View.VISIBLE);
+*/
+       // imageView.setVisibility(View.VISIBLE);
 
     }
 
